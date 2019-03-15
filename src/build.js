e88e07796eca7d665ca616fa0db28e8ffb17f279
clarkx86 <clarkx86@outlook.com>
2019-03-16 00:19:43 +0100

    Update .gitignore

--- src/build.js
+++ src/build.js
@@ -4,15 +4,13 @@ const path         = require( 'path' );
 
 var targets = 'node10-linux-x64,node10-win-x64';
 
-console.log( 'Building...' );
+console.log( 'Building ' + json_package.name.charAt( 0 ).toUpperCase() + json_package.name.slice( 1 ) + ' v' + json_package.version + json_package.version_devstate.charAt( 0 ) + ' (Targets: ' + targets + ')...' );
 
 var build = exec( 'pkg ' + json_package.main + ' --targets ' + targets + /*' --version ' + json_package.version + */ ' --output "' + path.normalize( path.join( __dirname, '../bin/' + json_package.name + '_' + json_package.version_devstate + json_package.version ) ) + '"', function( err, stdout, stderr ) {
-
     if ( err )
     {
         console.error( err );
         return;
     };
-
     console.log( stdout );
 } );
